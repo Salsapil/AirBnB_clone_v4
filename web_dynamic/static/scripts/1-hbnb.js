678d0244da4a30d7ef29a0df54af061e1cddfb20
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-31 21:17:13 +0300

    2. Select some Amenities to be comfortable!

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,26 @@
 $(document).ready(() => {
-    // let list = ["sdaff", "asdfdsaf", "asdfdsaf sdafads"]
-    let list = []
-    $("ul input").on('click', function (params) {
+    let amenities_ids = []
+    let amenities_names = []
+    $(".amenities ul input").on('click', function (params) {
         if ($(this).is(':checked')) {
-            list.push($(this).parent().attr('data-name'));
+            amenities_names.push($(this).parent().attr('data-name'));
+            amenities_ids.push($(this).parent().attr('data-id'));
         } else {
-            let q = list.indexOf($(this).parent().attr('data-name'))
+            let q = amenities_names.indexOf($(this).parent().attr('data-name'))
+            let i = amenities_ids.indexOf($(this).parent().attr('data-id'))
 
-            list.splice(q, 1)
+            amenities_names.splice(q, 1)
+            amenities_ids.splice(i, 1)
         }
-        console.log(list);
-        if (list.length > 0) {
-            $('.amenities h4').text(list.join(', '));
+        if (amenities_names.length > 0) {
+            $('.amenities h4').text(amenities_names.join(', '));
 
         } else {
             $('.amenities h4').html('&nbsp;');
 
         }
+        // console.log(amenities_ids)
+        // console.log(amenities_names)
     })
 
 });
